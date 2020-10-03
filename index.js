e04a5f82aa6a3a6197edabcd04e16c688843bc8f
Wei Zhang <iusenenow@gmail.com>
2020-10-03 22:52:34 +1000

    update

--- index.js
+++ index.js
@@ -38,12 +38,80 @@
 //   remove: () => { }
 // }
 
-let myModule = (function () {
-  let _data = []
-  let _render = function () { console.log('rendered') }
-  let _add = function () { }
-  let _remove = function () { }
-  return { render: _render }
+// let myModule = (function () {
+//   let _data = []
+//   let _render = function () { console.log('rendered') }
+//   let _add = function () { }
+//   let _remove = function () { }
+//   return { render: _render }
+// })()
+
+// myModule.render()
+
+// 4.Params: The "arguments" is only available in functions that are declared with "function" - either named or anonymous. A.K.A. you can't use "arguments" inside an arrow function
+// let f = function (a, b) {
+//   if (arguments.length === f.length) {
+//     console.log('We have a match')
+//   } else {
+//     console.log('No match')
+//   }
+// }
+
+// // determine if the function received the number of params it expected
+// f(1) // No match
+// f(1, 2) // We have a match
+// f(1, 2, 3) // No match
+
+// 5.ASI
+// ASI, object literals, square bracket syntax, hoisting
+// what happends when you run this code? why?
+// how would you fix it?
+
+// let a = { a: 123 };
+// [a].forEach(x => console.log(x))
+// output: a is not defined.
+// need a semicolon after let a = { a: 123}. Otherwise js will interprete the code as:
+// let a; //undefined (hoisting)
+// a = { a: 123 }[a].forEach(x => console.log(x))
+// a = { a: 123 }[undefined].forEach(x => console.log(x))
+
+/*
+  Create an example of a JavaScript Singleton
+  After the first object is created, it will return additional references to itself
+*/
+
+let obj = (function () {
+  let objInstance; // private variable
+  function create() {
+    // private function to create methods and properties
+    let _isRunning = false
+    let start = function () {
+      _isRunning = true
+    }
+    let stop = function () {
+      _isRunning = false
+    }
+    let currentState = function () {
+      return _isRunning
+    }
+    return {
+      start, stop, currentState
+    }
+  }
+  return {
+    getInstance: function () {
+      if (!objInstance) {
+        objInstance = create()
+      }
+      return objInstance
+    }
+  }
 })()
 
-myModule.render
+let obj1 = obj.getInstance()
+let obj2 = obj.getInstance()
+console.log(obj1.currentState())
+console.log(obj2.currentState())
+obj1.start()
+console.log(obj1.currentState())
+console.log(obj2.currentState())
